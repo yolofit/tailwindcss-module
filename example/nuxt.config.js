8a8ae57da193936c8192fc55df31dec816b17925
yolo <weixu416@gmail.com>
2021-09-04 00:29:46 +0800

    feat: example

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,12 +1,23 @@
 import tailwindModule from '../src'
 
 export default {
+  css: [
+    {
+      src: './global-order-1.css',
+      lang: 'css'
+    },
+    {
+      src: './global-order-2.css',
+      lang: 'css'
+    }
+  ],
   buildModules: [
     tailwindModule
   ],
   components: true,
   tailwindcss: {
     jit: true,
-    exposeConfig: true
+    exposeConfig: true,
+    injectPosition: 0
   }
 }
--- example/pages/index.vue
+++ example/pages/index.vue
@@ -3,7 +3,9 @@
     <div>
       <CallToAction />
     </div>
-
+    <div class="test text-4xl bg-gray-100">
+      override classname
+    </div>
     <pre class="p-2 m-4 bg-gray-100 rounded">{{ JSON.stringify(tailwindConfig, null, 2) }}</pre>
   </div>
 </template>
